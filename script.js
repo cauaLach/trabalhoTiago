9e1bc7c8c3ce56a1b6a3c0af6913a05ba61bcd24
Alessandra Penz <caualachlash@gmail.com>
2024-05-15 13:49:40 -0300

    CriacaoBascara

--- script.js
+++ script.js
@@ -8,3 +8,21 @@ botao.addEventListener("click",() =>{
         resultadoTabuada.innerHTML += "<p>" + (numero + "x" + i + ": "+ i*numero) + "<p>"
     }
 })
+
+var botao02 = document.getElementById("botao02")
+botao02.addEventListener("click", () => {
+    var valorA = document.getElementById("A")
+    var valorB = document.getElementById("B")
+    var valorC = document.getElementById("C")
+
+    var a = parseInt(valorA.value)
+    var b = parseInt(valorB.value)
+    var c = parseInt(valorC.value)
+
+    var delta = (b * b) - 4 * (a * c)
+    var bascara1 = ((0 - b) + Math.sqrt(delta))/(2*a)
+    var bascara2 = ((0 - b) - Math.sqrt(delta))/(2*a)
+
+    var resultadoBascara = document.getElementById("resultadoBascara")
+    resultadoBascara.innerHTML += "<p>"+ ("X¹: " + bascara1 + ", X²: " + bascara2) + "</p>"
+})
